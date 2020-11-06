fe0d58c306d706ad0ca8ed3eded53eef7407457a
ch31212y <okaoka1567@gmail.com>
2020-11-06 20:25:27 +0900

    init: firebase Authentication

--- server/golang/service/firebaseAuth.go
+++ server/golang/service/firebaseAuth.go
@@ -1,1 +1,26 @@
 package service
+
+import (
+	"context"
+	firebase "firebase.google.com/go"
+	firebaseAuth "firebase.google.com/go/auth"
+	"fmt"
+	"google.golang.org/api/option"
+	"os"
+)
+
+var auth *firebaseAuth.Client
+
+// init firebase authentication
+func init() {
+	ctx := context.Background()
+	opt := option.WithCredentialsFile(os.Getenv("FIREBASE_SECRET"))
+	app, err := firebase.NewApp(ctx, nil, opt)
+	if err != nil {
+		panic(fmt.Errorf("error on init firebase auth: %v", err))
+	}
+	auth, err = app.Auth(ctx)
+	if err != nil {
+		panic(fmt.Errorf("error on get auth %v", err))
+	}
+}

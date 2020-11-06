fef5e448ee93f389f5dbddc054025406cc664522
ch31212y <okaoka1567@gmail.com>
2020-11-06 20:48:39 +0900

    add: grpc file genearator

---
+++ server/gen-protoc.bat
@@ -0,0 +1,6 @@
+protoc --go_out=plugins=grpc:./golang/TalkRpc chat.proto
+python -m grpc.tools.protoc -I. --python_out=../client/python/lib/api --grpc_python_out=../client/python/lib/api chat.proto
+
+git add ./golang/TalkRpc/
+git add ../client/python/lib/api/
+git commit -m "auto: grpc generated commit"

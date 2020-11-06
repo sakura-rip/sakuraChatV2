22dbc7c13005350415c69d34635f9d7dee8751f1
ch31212y <okaoka1567@gmail.com>
2020-11-07 00:42:34 +0900

    init: auth.py

--- client/python/lib/auth.py
+++ client/python/lib/auth.py
@@ -1,3 +1,10 @@
+from .api.chat_pb2_grpc import AuthServiceStub
+
+
 class Auth:
     def __init__(self):
+        self.auth = AuthServiceStub(self.channel)
+
+    def create_account(self):
+        sessionID = self.auth.CreateRegisterSession()
         pass

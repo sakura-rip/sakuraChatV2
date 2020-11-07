cbe23e5370decbb0e4682fc1e521e214eab2ddc4
ch31212y <okaoka1567@gmail.com>
2020-11-07 11:58:05 +0900

    init: auth

--- client/python/lib/main.py
+++ client/python/lib/main.py
@@ -1,12 +1,16 @@
+from client.python.lib.auth import Auth
 import grpc
 from .config import Config
 from .talk import Talk
+from .auth import Auth
 
 
-class ChatClient(Talk):
+class ChatClient(Talk, Auth):
     def __init__(self):
         self.channel = grpc.insecure_channel(Config.HOST_URL)
         self.__init_all()
 
     def __init_all(self):
         Talk.__init__(self)
+        Auth.__init__(self)
+

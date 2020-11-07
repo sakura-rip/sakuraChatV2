ddf1c4db0d947e09c22c1c277c095e206bf92050
ch31212y <okaoka1567@gmail.com>
2020-11-07 18:26:50 +0900

    init: VerifyIDToken func

--- server/golang/service/auth.go
+++ server/golang/service/auth.go
@@ -1,1 +1,11 @@
 package service
+
+import (
+	"context"
+	"github.com/ch31212y/sakuraChatV2/TalkRPC"
+	"google.golang.org/grpc"
+)
+
+func (cl *AuthHandler) VerifyIDToken(ctx context.Context, in *TalkRPC.VerifyIDTokenRequest, opts ...grpc.CallOption) (*TalkRPC.VerifyIDTokenResponse, error) {
+
+}

1cb2c12644d0abb62ae0cad1c22a3441c22baefd
ch31212y <okaoka1567@gmail.com>
2020-11-08 09:13:40 +0900

    add: VerifyIDToken func

--- server/golang/service/auth.go
+++ server/golang/service/auth.go
@@ -4,8 +4,27 @@ import (
 	"context"
 	"github.com/ch31212y/sakuraChatV2/TalkRPC"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 )
 
 func (cl *AuthHandler) VerifyIDToken(ctx context.Context, in *TalkRPC.VerifyIDTokenRequest, opts ...grpc.CallOption) (*TalkRPC.VerifyIDTokenResponse, error) {
-
+	jwt, err := auth.VerifyIDToken(ctx, in.JwtToken)
+	if err != nil {
+		return &TalkRPC.VerifyIDTokenResponse{}, status.New(codes.InvalidArgument, "bad token").Err()
+	}
+	claims := map[string]interface{}{
+		"premium": false,
+	}
+	//premiumカラムをつけてToken 再発行
+	token, err := auth.CustomTokenWithClaims(ctx, jwt.UID, claims)
+	if err != nil {
+		return &TalkRPC.VerifyIDTokenResponse{}, status.New(codes.Internal, "internal error").Err()
+	}
+	//クライアントのヘッダーを追加
+	_ = grpc.SetHeader(ctx, metadata.Pairs("x-chat-token", token))
+	return &TalkRPC.VerifyIDTokenResponse{
+		JwtToken: token,
+	}, nil
 }

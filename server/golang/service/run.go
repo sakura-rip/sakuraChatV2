6e41ba3d2f873ea44482b91aa103a976acaf87af
ch31212y <okaoka1567@gmail.com>
2020-11-09 19:40:28 +0900

    add: mongoDB collection

--- server/golang/service/run.go
+++ server/golang/service/run.go
@@ -1,6 +1,9 @@
 package service
 
-import "github.com/ch31212y/sakuraChatV2/database"
+import (
+	"github.com/ch31212y/sakuraChatV2/database"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 type TalkHandler struct{}
 
@@ -8,6 +11,9 @@ type AuthHandler struct{}
 
 var db *database.DBClient
 
+var userDB *mongo.Collection
+
 func init() {
 	db.Session = database.ConnectToMongoDB()
+	userDB = db.Session.Database("sakuraChat").Collection("users")
 }

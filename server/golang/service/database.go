37dd756c4fd31f3ca3def557f00d7f3c4e1971b0
ch31212y <okaoka1567@gmail.com>
2020-11-10 12:47:09 +0900

    add: findUserFromDB func

---
+++ server/golang/service/database.go
@@ -0,0 +1,22 @@
+package service
+
+import (
+	"github.com/ch31212y/sakuraChatV2/database"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func findUserFromDB(id string, projection bson.D) (*database.User, error) {
+	rs := userDB.FindOne(
+		ctx,
+		bson.D{{"_id", id}},
+		options.FindOne().SetProjection(projection),
+	)
+	var usr *database.User
+	if rs.Decode(&usr) != nil {
+		return usr, status.New(codes.NotFound, "user not found").Err()
+	}
+	return usr, nil
+}

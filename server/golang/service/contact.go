c768968697e5e9f89e049163755ffb5f92f96c4b
ch31212y <okaoka1567@gmail.com>
2020-11-12 19:49:30 +0900

    add: rpc: getContact

--- server/golang/service/contact.go
+++ server/golang/service/contact.go
@@ -3,9 +3,23 @@ package service
 import (
 	"context"
 	"github.com/ch31212y/sakuraChatV2/TalkRPC"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (cl TalkHandler) GetContact(ctx context.Context, in *TalkRPC.GetContactRequest) (*TalkRPC.GetContactResponse, error) {
+	uuid, ok, _ := VerifyTokenAndGetUUID(ctx)
+	if ok == false {
+		return nil, status.New(codes.Unauthenticated, "Invalid Token").Err()
+	}
+	dbContact, err := findContactFromDB(uuid, in.UUID)
+	if err != nil {
+		return nil, status.New(codes.NotFound, "target not found").Err()
+	}
+	contact := TalkRPC.GetContactResponse{
+		Contact: &TalkRPC.Contact{},
+	}
+	return &contact, nil
 
 }
 func (cl TalkHandler) GetContacts(ctx context.Context, in *TalkRPC.GetContactsRequest) (*TalkRPC.GetContactsResponse, error) {

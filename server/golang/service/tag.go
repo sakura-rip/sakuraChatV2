dcc2e479bce0a8caf819a2aa4993d52a6c19bd48
ch31212y <okaoka1567@gmail.com>
2020-11-13 16:19:41 +0900

    add: rpc: Tag func

---
+++ server/golang/service/tag.go
@@ -0,0 +1,18 @@
+package service
+
+import (
+	"context"
+	"github.com/ch31212y/sakuraChatV2/TalkRPC"
+)
+
+func (cl TalkHandler) GetAllTags(ctx context.Context, in *TalkRPC.GetAllTagsRequest) (*TalkRPC.GetAllTagsResponse, error) {
+
+}
+
+func (cl TalkHandler) CreateTag(ctx context.Context, in *TalkRPC.CreateTagRequest) (*TalkRPC.CreateTagResponse, error) {
+
+}
+
+func (cl TalkHandler) DeleteTag(ctx context.Context, in *TalkRPC.DeleteTagRequest) (*TalkRPC.DeleteTagResponse, error) {
+
+}

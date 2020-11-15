dbce435e8d77f896d1239532e588669d19e26445
ch31212y <okaoka1567@gmail.com>
2020-11-15 20:47:42 +0900

    OTW: BlockContact

--- server/golang/service/contact.go
+++ server/golang/service/contact.go
@@ -2,7 +2,10 @@ package service
 
 import (
 	"context"
+	"fmt"
 	"github.com/ch31212y/sakuraChatV2/TalkRPC"
+	"github.com/ch31212y/sakuraChatV2/database"
+	"go.mongodb.org/mongo-driver/bson"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -43,6 +46,37 @@ func (cl TalkHandler) BlockContact(ctx context.Context, in *TalkRPC.BlockContact
 	if ok == false {
 		return nil, status.New(codes.Unauthenticated, "Invalid Token").Err()
 	}
+	contact, err := findRPCContactFromDB(uuid, in.UUID)
+	if err != nil {
+		return nil, err
+	}
+	switch contact.Status {
+	case TalkRPC.FriendStatus_friend:
+		_, _ := userCol.UpdateOne(
+			ctx,
+			bson.M{"_id": uuid},
+			"", //https://docs.mongodb.com/manual/reference/operator/update/pull/
+		)
+	case TalkRPC.FriendStatus_delete:
+	case TalkRPC.FriendStatus_block:
+	case TalkRPC.FriendStatus_nothing:
+	}
+	updateAttr := bson.M{"$set": bson.M{
+		fmt.Sprintf("contacts.%d", in.UUID): database.Contact{
+			UUID:            in.UUID,
+			OverWrittenName: contact.OverWrittenName,
+			Status:          2,
+			TagIds:          contact.TagIds,
+		}},
+	}
+	if _, err := userCol.UpdateOne(
+		ctx,
+		bson.M{"_id": uuid},
+		updateAttr,
+	); err != nil {
+		return nil, status.New(codes.NotFound, "no contact found").Err()
+	}
 }
+
 func (cl TalkHandler) ReportContact(ctx context.Context, in *TalkRPC.ReportContactRequest) (*TalkRPC.ReportContactResponse, error) {
 }

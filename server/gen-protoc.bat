05f2165dc4995d795f542f9e448095cb9548f785
ch31212y <okaoka1567@gmail.com>
2020-11-16 13:13:43 +0900

    add: java gen file

--- server/gen-protoc.bat
+++ server/gen-protoc.bat
@@ -1,6 +1,8 @@
 protoc --go_out=plugins=grpc:./golang/TalkRpc chat.proto
 python -m grpc.tools.protoc -I. --python_out=../client/python/lib/api --grpc_python_out=../client/python/lib/api chat.proto
 
+protoc -I=. chat.proto --js_out=import_style=commonjs:../client/web --grpc-web_out=import_style=commonjs+dts,mode=grpcwebtext:../client/web --plugin=protoc-gen-grpc-web="protoc-gen-grpc-web-1.2.1-windows-x86_64.exe"
+
 git add golang/TalkRPC/chat.pb.go
 git add ../client/python/lib/api/
 git commit -m "auto: grpc generated commit"

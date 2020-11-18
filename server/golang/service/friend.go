67a8adbd5b5e9f7d2430eb8440111b49d633c8ab
ch31212y <okaoka1567@gmail.com>
2020-11-18 13:19:07 +0900

    add: auth check

--- server/golang/service/friend.go
+++ server/golang/service/friend.go
@@ -3,9 +3,15 @@ package service
 import (
 	"context"
 	"github.com/ch31212y/sakuraChatV2/TalkRPC"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (cl TalkHandler) SendFriendRequest(ctx context.Context, in *TalkRPC.SendFriendRequestRequest) (*TalkRPC.SendFriendRequestResponse, error) {
+	uuid, ok, _ := VerifyTokenAndGetUUID(ctx)
+	if ok == false {
+		return nil, status.New(codes.Unauthenticated, "Invalid Token").Err()
+	}
 
 }
 func (cl TalkHandler) AcceptFriendRequest(ctx context.Context, in *TalkRPC.AcceptFriendRequestRequest) (*TalkRPC.AcceptFriendRequestResponse, error) {

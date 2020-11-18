208c0aea5c08e9ccf393379e389647803e2c8be7
ch31212y <okaoka1567@gmail.com>
2020-11-18 14:32:49 +0900

    add: rpc: SendFriendRequest

--- server/golang/service/friend.go
+++ server/golang/service/friend.go
@@ -3,8 +3,11 @@ package service
 import (
 	"context"
 	"github.com/ch31212y/sakuraChatV2/TalkRPC"
+	"github.com/ch31212y/sakuraChatV2/database"
+	"go.mongodb.org/mongo-driver/bson"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"time"
 )
 
 func (cl TalkHandler) SendFriendRequest(ctx context.Context, in *TalkRPC.SendFriendRequestRequest) (*TalkRPC.SendFriendRequestResponse, error) {
@@ -12,7 +15,30 @@ func (cl TalkHandler) SendFriendRequest(ctx context.Context, in *TalkRPC.SendFri
 	if ok == false {
 		return nil, status.New(codes.Unauthenticated, "Invalid Token").Err()
 	}
-
+	field := bson.M{"FriendRequests." + in.UUID: database.FriendRequest{
+		FromID:      uuid,
+		ToID:        in.UUID,
+		CreatedTime: time.Now().Unix(),
+		Message:     in.Message,
+	}}
+	_, dbErr := userCol.UpdateOne(
+		ctx,
+		bson.M{"_id": uuid},
+		bson.M{"$set": field},
+	)
+	if dbErr != nil {
+		return nil, status.New(codes.Internal, "DB error").Err()
+	}
+	//受信したもの
+	_, dbErr2 := userCol.UpdateOne(
+		ctx,
+		bson.M{"_id": in.UUID},
+		bson.M{"$set": field},
+	)
+	if dbErr2 != nil {
+		return nil, status.New(codes.Internal, "db error").Err()
+	}
+	return &TalkRPC.SendFriendRequestResponse{}, nil
 }
 func (cl TalkHandler) AcceptFriendRequest(ctx context.Context, in *TalkRPC.AcceptFriendRequestRequest) (*TalkRPC.AcceptFriendRequestResponse, error) {
 

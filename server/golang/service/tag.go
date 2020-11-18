2bca0d529008559077fd0b204c548c79a0056ccb
ch31212y <okaoka1567@gmail.com>
2020-11-18 11:26:27 +0900

    add: rpc: CreateTag
    Tagの編集にも用いる

--- server/golang/service/tag.go
+++ server/golang/service/tag.go
@@ -3,6 +3,10 @@ package service
 import (
 	"context"
 	"github.com/ch31212y/sakuraChatV2/TalkRPC"
+	"github.com/ch31212y/sakuraChatV2/database"
+	"go.mongodb.org/mongo-driver/bson"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (cl TalkHandler) GetAllTags(ctx context.Context, in *TalkRPC.GetAllTagsRequest) (*TalkRPC.GetAllTagsResponse, error) {
@@ -10,6 +14,27 @@ func (cl TalkHandler) GetAllTags(ctx context.Context, in *TalkRPC.GetAllTagsRequ
 }
 
 func (cl TalkHandler) CreateTag(ctx context.Context, in *TalkRPC.CreateTagRequest) (*TalkRPC.CreateTagResponse, error) {
+	uuid, ok, _ := VerifyTokenAndGetUUID(ctx)
+	if ok == false {
+		return nil, status.New(codes.Unauthenticated, "Invalid Token").Err()
+	}
+	_, dbError := userCol.UpdateOne(
+		ctx,
+		bson.M{"_id": uuid},
+		bson.D{
+			{"$set", bson.D{{"tags." + in.Tag.TagID, database.Tag{
+				TagID:       in.Tag.TagID,
+				Name:        in.Tag.Name,
+				Description: in.Tag.Description,
+				Color:       in.Tag.Color,
+				CreatorUUID: in.Tag.CreatorUUID,
+				CreatedTime: in.Tag.CreatedTime,
+			}}}},
+		})
+	if dbError != nil {
+		return nil, status.New(codes.Internal, "db error").Err()
+	}
+	return &TalkRPC.CreateTagResponse{}, nil
 
 }
 
